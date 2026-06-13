7ec9e950d7d53d63c55d88a2ad03e82a17ad4b89
nanurodriguez <nancy95rod@gmail.com>
2026-06-13 17:59:15 -0600

    updating vite

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,8 @@ export default defineConfig({
     outDir: '../dist',
     rollupOptions: {
       input: {
-        // Set your main landing page as the primary entry point
         main: resolve(__dirname, 'src/index.html'),
         
-        // If you have a separate login page or user profile page created, 
-        // you can include them below like this:
-        // login: resolve(__dirname, 'src/login/index.html')
       }
     }
   }
