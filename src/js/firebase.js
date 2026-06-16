1f4e6f71bc31e826318d8c98c83bc8092951d780
nanurodriguez <nancy95rod@gmail.com>
2026-06-16 16:03:43 -0600

    Connecting live Google Cloud Firebase to my project

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -7,12 +7,13 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth
 
 // Importante!! Replace these placeholder strings with  actual Firebase project web app credentials!
 const firebaseConfig = {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_AUTH_DOMAIN',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_STORAGE_BUCKET',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId: 'YOUR_APP_ID'
+    apiKey: 'AIzaSyATDGKfmDYOjpX_F0oupYhFsavcX7hxz4Y',
+    authDomain: 'nook-app-117aa.firebaseapp.com',
+    projectId: 'nook-app-117aa',
+    storageBucket: 'nook-app-117aa.firebasestorage.app',
+    messagingSenderId: '377008555208',
+    appId: '1:377008555208:web:2ddc9d53c4cd47811451fd',
+    measurementId: 'G-R5XF04T9N3'
 };
 
 // Initialize Firebase services
