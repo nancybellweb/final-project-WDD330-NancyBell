e56f6d9bab119ff68bb93111ee919d6d602b6b25
nanurodriguez <nancy95rod@gmail.com>
2026-06-17 10:57:10 -0600

    adding base to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
+// vite.config.js
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src/',
+  base: '/', 
+  publicDir: '../public', 
   build: {
     outDir: '../dist',
+    emptyOutDir: true, 
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
         login: resolve(__dirname, 'src/login/index.html'),
-        dashboard: resolve(__dirname, 'src/dashboard/index.html'), 
+        dashboard: resolve(__dirname, 'src/dashboard/index.html'),
         goals: resolve(__dirname, 'src/goals/index.html'),
         notes: resolve(__dirname, 'src/notes/index.html'),
-        profile: resolve(__dirname, 'src/profile/index.html'),
       },
     },
   },
